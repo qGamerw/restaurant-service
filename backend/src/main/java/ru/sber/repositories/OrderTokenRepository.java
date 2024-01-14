5cf37bcd2365e005d8292406986d029a6e359fa4
Misha Ukhin <mish.uxin2012@yandex.ru>
2024-01-14 14:49:23 +0300

    refactoring the code, functionality was not tested

--- backend/src/main/java/ru/sber/repositories/OrderTokenRepository.java
+++ backend/src/main/java/ru/sber/repositories/OrderTokenRepository.java
@@ -2,7 +2,6 @@ package ru.sber.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.sber.entities.BranchOffice;
 import ru.sber.entities.OrderToken;
 
 /**
