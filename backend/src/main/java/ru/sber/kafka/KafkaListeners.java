15fb41745ff9c1f0e338fe2e33278c318c3f0a4d
Misha Ukhin <mish.uxin2012@yandex.ru>
2024-03-31 20:56:16 +0300

    The ultimate vision of this package kafka

--- backend/src/main/java/ru/sber/kafka/KafkaListeners.java
+++ backend/src/main/java/ru/sber/kafka/KafkaListeners.java
@@ -1,6 +1,5 @@
 package ru.sber.kafka;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -20,18 +19,30 @@ public class KafkaListeners {
         this.notifyServiceImp = notifyServiceImp;
     }
 
+    /**
+     * Читает информацию из темы review_order в Kafka
+     *
+     * @param data прочитанные данные
+     */
     @KafkaListener(topics = "review_order", groupId = "selfMessages")
     void listenerReviewOrder(String data) {
+        //TODO: уже не помню зачем он нужен.
         log.info("Listener получил: {}", data);
     }
 
+    /**
+     * Читает информацию из темы restaurant_status в Kafka
+     *
+     * @param data прочитанные данные
+     */
     @KafkaListener(topics = "restaurant_status", groupId = "selfMessages")
-    void listenerRestaurantStatus(String data) throws JsonProcessingException {
-        log.info("Listener получил: {}", data);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Order order = objectMapper.readValue(data, Order.class);
-        log.info("Order: {}", order);
-        notifyServiceImp.addNotify(order.getId());
+    void listenerRestaurantStatus(String data) {
+        try {
+            Order order = new ObjectMapper().readValue(data, Order.class);
+            log.info("Order: {}", order);
+            notifyServiceImp.addNotify(order.getId());
+        } catch (com.fasterxml.jackson.core.JsonProcessingException e) {
+            log.error("Ошибка чтения: Не удалось прочитать полученные данные.");
+        }
     }
 }
